bd5789005ffb350d28ea642ff4cebd8c47193402
konotori <tungto2010@gmail.com>
2021-08-27 18:01:02 +0700

    OOP example

---
+++ OOP/OOP.playground/Contents.swift
@@ -0,0 +1,101 @@
+import UIKit
+
+
+class Music {
+    let notes: [String]
+    
+    init(notes: [String]) {
+        self.notes = notes
+    }
+    
+    func prepared() -> String {
+        return notes.joined(separator: " ")
+    }
+}
+
+
+// 1
+class Instrument {
+    // 2
+    let brand: String
+    // 3
+    init(brand: String) {
+        // 4
+        self.brand = brand
+    }
+    
+    func tune() -> String {
+        fatalError("Implement this method for \(brand)")
+    }
+    
+    func play(_ music: Music) -> String {
+        return music.prepared()
+    }
+    
+    func perform(_ music: Music) {
+        print(tune())
+        print(play(music))
+    }
+}
+
+// 1.INHERIT
+class Piano: Instrument {
+    let hasPedals: Bool
+    // 2
+    static let whiteKeys = 52
+    static let blackKeys = 36
+    
+    // 3
+    init(brand: String, hasPedals: Bool = false) {
+        self.hasPedals = hasPedals
+        // 4
+        super.init(brand: brand)
+    }
+    
+    // 5
+    override func tune() -> String {
+        return "Piano standard tuning for \(brand)."
+    }
+    
+    override func play(_ music: Music) -> String {
+        return play(music, usingPedals: hasPedals)
+    }
+    
+    
+    // 2. Overload
+    func play(_ music: Music, usingPedals: Bool) -> String {
+        let preparedNotes = super.play(music)
+        if hasPedals && usingPedals {
+            return "Play piano notes \(preparedNotes) with pedals."
+        }
+        else {
+            return "Play piano notes \(preparedNotes) without pedals."
+        }
+    }
+}
+
+
+// 1
+let piano = Piano(brand: "Yamaha", hasPedals: true)
+piano.tune()
+// 2
+let music = Music(notes: ["C", "G", "F"])
+piano.play(music, usingPedals: false)
+// 3
+piano.play(music)
+// 4
+Piano.whiteKeys
+Piano.blackKeys
+
+class Guitar: Instrument {
+  let stringGauge: String
+  
+  init(brand: String, stringGauge: String = "medium") {
+    self.stringGauge = stringGauge
+    super.init(brand: brand)
+  }
+    
+    
+}
+
+
